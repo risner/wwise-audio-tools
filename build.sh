8cf7154197720ed569d858dacc8bc62690318613
James Risner <risner@lokkak.com>
2025-11-10 13:13:07 -0500

    Strip the macOS library, saves 800kb.

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 # Build for macOS
-rm -rf build obj && mkdir build && mkdir obj && (cd build && cmake .. && make)
+rm -rf build obj && mkdir build && mkdir obj && cmake -DCMAKE_BUILD_TYPE=Release -S . -B build && \
+	cmake --build build -- -j`sysctl -n hw.logicalcpu`
